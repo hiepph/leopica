febedd5ec66e4ddc76a5bc4b81bd3957d58ad835
hiepph <hoanghiepjp96@gmail.com>
2017-01-07 08:33:46 +0000

    test(plot): Plot with data from gy_521

--- sensors/plot.py
+++ sensors/plot.py
@@ -1,12 +1,14 @@
+import datetime 
+import time   
+import gy_521
 import plotly
 import numpy as np 
 import plotly.plotly as py  
 import plotly.tools as tls   
 import plotly.graph_objs as go
 
-stream_ids = tls.get_credentials_file()['stream_ids']
-
 # Get stream id from stream id list 
+stream_ids = tls.get_credentials_file()['stream_ids']
 stream_id = stream_ids[0]
 
 # Make instance of stream id object 
@@ -24,24 +26,22 @@ trace1 = go.Scatter(
     mode='lines+markers',
     stream=stream_1         # (!) embed stream id, 1 per trace
 )
-
 data = go.Data([trace1])
+
 # Add title to layout object
-layout = go.Layout(title='Time Series')
+layout = go.Layout(title='Rotation status')
 
 # Make a figure object
 fig = go.Figure(data=data, layout=layout)
 
 # Send fig to Plotly, initialize streaming plot, open new tab
-py.iplot(fig, filename='python-streaming')
+py.plot(fig, filename='gy-521')
+
 # We will provide the stream link object the same token that's associated with the trace we wish to stream to
 s = py.Stream(stream_id)
 
 # We then open a connection
 s.open()
-# (*) Import module keep track and format current time
-import datetime 
-import time   
  
 i = 0    # a counter
 k = 5    # some shape parameter
@@ -50,18 +50,17 @@ k = 5    # some shape parameter
 time.sleep(5) 
 
 while True:
-    
-    # Current time on x-axis, random numbers on y-axis
-    x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    y = (np.cos(k*i/50.)*np.cos(i/50.)+np.random.randn(1))[0] 
+    [x, y] = gy_521.rotation_data()
         
-    # Send data to your plot
+    # Send data to plot
     s.write(dict(x=x, y=y))  
     
     #     Write numbers to stream to append current data on plot,
     #     write lists to overwrite existing data on plot
             
-    time.sleep(1)  # plot a point every second    
+    # Plot with 100Hz frequency
+    time.sleep(1) 
+
 # Close the stream when done plotting
 s.close() # Embed never-ending time series streaming plot
-tls.embed('streaming-demos','12')
+tls.embed('gy-521','12')
