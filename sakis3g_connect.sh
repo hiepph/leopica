ef8792e4b5b74ac7d5eade321d98ee3e51fe8941
hiepph <hoanghiepjp96@gmail.com>
2017-01-07 19:03:10 +0000

    refactor(3g): Add logger info

--- sakis3g_connect.sh
+++ sakis3g_connect.sh
@@ -1,2 +1,7 @@
 # Activate 3G DGCOM Viettel
-sakis3g --console connect APN='e-connect' APN_USER="0" APN_PASS='0'  
+# 
+# With Raspberry Pi, you can easily put it in /etc/rc.local for 
+# auto starting after booting
+
+printf "INFO: Auto connecting to 3G dongle\n"
+sakis3g --console connect APN='e-connect' APN_USER='0' APN_PASS='0'
