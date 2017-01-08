be9d1c465e5473dfe7674f5417a1c34bfa73ea87
hiepph <hoanghiepjp96@gmail.com>
2017-01-08 09:49:17 +0000

    refactor(leopica): Better way for continuos image

--- streaming/live.sh
+++
@@ -1,1 +0,0 @@
-raspivid -o - -t 0 -fps 30 -b 8000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7p25-7jg0-ph9y-8xfz
