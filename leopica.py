83fc3e5718d50d200ecfcfd9c3476c2f5b5bb1ab
hiepph <hoanghiepjp96@gmail.com>
2017-01-08 12:20:23 +0000

    feat: Integrate Redis worker for handling image upload

---
+++ leopica.py
@@ -0,0 +1,34 @@
+import time
+import logging
+import picamera
+from upload import Upload
+from redis import Redis
+from rq import Queue
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s ($%(name)s) - %(message)s',
+                    level=logging.DEBUG)
+
+def shoot_and_upload_images(uploader, delay=5):
+    with picamera.PiCamera() as camera:
+        logging.info("Setting up camera.")
+        # 1s shutter speed
+        camera.shutter_speed = 1000000
+        # camera.start_preview()
+
+        # Wait for the automatic gain control to settle
+        time.sleep(5)
+        
+        logging.info("Start photographing!")
+        # Start capturing continuosly
+        for image in camera.capture_continuous('images/img-{timestamp:%Y-%m-%d-%H:%M:%S}.jpg'):
+            logging.info("Captured %s" % image)
+            q.enqueue(upload.upload_to_drive, image)
+            time.sleep(delay)
+
+if __name__ == "__main__":
+    q = Queue(connection=Redis())
+    
+    logging.info("Login to Drive Account")
+    upload = Upload()
+
+    shoot_and_upload_images(upload, 1)
